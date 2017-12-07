b66d62fccd3b2a3ad2175d4576c87f159648abf6
tiamilani <mattia.milani10@gmail.com>
2017-12-07 10:28:47 +0100

    .

--- api/controllers/student.js
+++ api/controllers/student.js
@@ -18,7 +18,7 @@ exports.addStudent = function(req, response) {
 
     assignments.push(json);
 
-	response.end(json);
+	response.end(JSON.stringify(json));
 };
 
 exports.modifyStudent = function(req, response) {
